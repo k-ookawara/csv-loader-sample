a0bca1eb6a452524b28398c1618f90b07344d848
okbabata <babata@wapio.jp>
2023-08-03 22:55:12 +0900

    検証用テーブルのDDLを変更

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,5 +1,14 @@
-create table pdbadmin.employee (
-    name varchar2(255),
-    age number(2),
-    birthday varchar2(10)
+create database mydb;
+
+create table mydb.employee (
+    col1 varchar(255),
+    col2 varchar(255),
+    col3 varchar(255),
+    col4 varchar(255),
+    col5 varchar(255),
+    col6 varchar(255),
+    col7 varchar(255),
+    col8 varchar(255),
+    col9 varchar(255),
+    col10 varchar(255)
 );
